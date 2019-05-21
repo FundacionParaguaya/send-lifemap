b01106d15195ed7b686a06cdd79c60aeb4a9cfc4
beamandymess <mandymcr88@gmail.com>
2019-05-21 16:57:17 -0400

    add error if number is not in db

--- database.py
+++ database.py
@@ -3,14 +3,19 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://LifeMap:helloworld@cluster0-6nhz3.mongodb.net/test?retryWrites=true')
 #putting the name of the database in mongo here
 db=client.PovertyStopLight
-def query_db(phone_number):
-    # .Family needs to be the name of the collection where the registers are loaded
-    register= db.Family.find_one({'phoneNumber': phone_number})
-    print(register)
-    if 'lifeMap' in register: 
-       send_lifemap(register['lifeMap']) #this function has to be the one that sends the pdf to the WA
 
+def query_db(phone_number):
+    register= db.Family.find_one({'phoneNumber': phone_number}) # .Family needs to be the name of the collection where the registers are loaded
+    if register :
+        print(register)
+        if 'lifeMap' in register: 
+            print(register['lifeMap'])
+            send_lifemap(register['lifeMap']) #this function has to be the one that sends the pdf to the WA
+        else:
+            pdf=generatepdf(register)
+            #saves the pdf generated file in blob to save it in the db 
+            register1=db.Family.update_one({'phoneNumber': phone_number},{'$set': {'lifeMap' : pdf}}) 
     else:
-        pdf=generatepdf()
-        #saves the pdf generated file in blob to save it in the db 
-        register1=db.Family.update_one({'phoneNumber': phone_number},{'$set': {'lifeMap' : pdf}}) 
+        send_not_in_db_message() #the function sends a message that the number is not in the database and should be checked
+        print("error")
+
