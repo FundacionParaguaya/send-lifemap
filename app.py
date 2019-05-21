bc7018677b956182be9df7187631b14616e9efe1
Bryan Giger <bryan@gigerbyt.es>
2019-05-21 19:09:52 -0400

    create render image system

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_file
 from connect_database import connect_mongo
-from twilio_helpers import send_template, send_messages
+# from twilio_helpers import send_template, send_messages
 import pdfkit
 
 app = Flask(__name__)
@@ -9,12 +9,10 @@ app = Flask(__name__)
 def hello_world():
     # db = connect_mongo()
     # family = db['family']
-
     # lifemap_data = json.dumps(list(family.find()))
     # send_lifemap(lifemap_data)
     send_lifemap('data/snapshot_sample.json')
     return render_template('grafic.html')
-
     # return str(list(family.find()))
 
 @app.route('/send-initial-message', methods = ['POST'])
@@ -27,10 +25,25 @@ def send_inital_message():
 def send_reminder():
     return jsonify(send_messages())
 
-@app.route('/send-lifemap', methods = ['POST'])
-def send_lifemap(data):
+@app.route('/send-lifemap', methods = ['GET'])
+def send_lifemap():
     # template = render_template('lifemap.html', json_data=data)
     # template = template.encode('utf-8')
-    return pdfkit.from_file('templates/grafic.html', "lifemap.pdf")
+    pdf = pdfkit.from_url('127.0.0.1:5000/render-template', 'output.pdf')
+    return jsonify("success")
 
     # deliver_via_whatsapp(lifemap)
+@app.route('/render-template', methods=["GET"])
+def display_graphic():
+    return render_template('grafic.html')
+
+# template = get_template("output_pdf.html")
+# context = Context({"data": data})  # data is the context data that is sent to the html file to render the output. 
+# html = template.render(context)  # Renders the template with the context data.
+# pdfkit.from_string(html, 'out.pdf')
+# pdf = open("out.pdf")
+# response = HttpResponse(pdf.read(), content_type='application/pdf')  # Generates the response as pdf response.
+# response['Content-Disposition'] = 'attachment; filename=output.pdf'
+# pdf.close()
+# os.remove("out.pdf")  # remove the locally created pdf file.
+# return response  # returns the response.
