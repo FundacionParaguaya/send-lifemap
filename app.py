f64b82bdb1aaf5f56c31b6c35d437e6ee4f4a1d9
Bryan Giger <bryan@gigerbyt.es>
2019-05-21 19:56:23 -0400

    creante get_lifemap function and integrate to server

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from connect_database import connect_mongo
+from connect_database import connect_mongo, get_lifemap
 from twilio_helpers import send_template, send_messages
 app = Flask(__name__)
 
@@ -7,15 +7,23 @@ app = Flask(__name__)
 def hello_world():
     db = connect_mongo()
     family = db['family']
-
     return str(list(family.find()))
 
 @app.route('/send-initial-message', methods = ['POST'])
 def send_inital_message():
     from_number = request.form['from']
-
     return jsonify(send_template(from_number))
 
 @app.route('/send-reminders', methods = ['POST'])
 def send_reminder():
     return jsonify(send_messages())
+
+@app.route('/send-lifemap', methods=['GET','POST'])
+def send_lifemap():
+    phone_number = '+595000000'
+    lifemap = get_lifemap(phone_number)
+    return lifemap
+
+@app.route('/render-template', methods['GET','POST'])
+def render_graphic():
+    return send_template("grafic.html")
--- connect_database.py
+++ connect_database.py
@@ -13,3 +13,21 @@ def connect_mongo():
     mongo_client = pymongo.MongoClient('mongodb+srv://'+ str(mongodb_user) +':' + str(mongodb_dbpass) + '@'+ str(mongodb_host)+'/' +str(mongodb_dbname)+'?retryWrites=true')
     db = mongo_client[str(mongodb_dbname)]
     return db
+
+
+def get_lifemap(phone_number):
+    db = connect_mongo()
+    familyCol = db['family']
+    family = familyCol.find_one({'phoneNumber': phone_number}) # .Family needs to be the name of the collection where the familys are loaded
+    if family :
+        print(family)
+        if 'lifeMap' in family: 
+            print(family['lifeMap'])
+            # ** send_lifemap(family['lifeMap']) #this function has to be the one that sends the pdf to the WA
+        else:
+            # ** pdf=generatepdf(family)
+            # ** saves the pdf generated file in blob to save it in the db 
+            familyCol.update_one({'phoneNumber': phone_number},{'$set': {'lifeMap' : pdf}}) 
+    else:
+        return "The famiy is not in the database" #the function sends a message that the number is not in the database and should be checked
+        print("error")
