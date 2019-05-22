b990050e698fb7c8e541bf20977eb605f84ca9a7
Bryan Giger <bryan@gigerbyt.es>
2019-05-21 20:18:13 -0400

    SID and AUTH as os.getenv

--- twilio_helpers.py
+++ twilio_helpers.py
@@ -1,5 +1,9 @@
-from secrets import SID, AUTH
+# from secrets import SID, AUTH
+import os
 from twilio.rest import Client
+
+SID = os.getenv("SID")
+AUTH = os.getenv("AUTH")
 
 POVERTY_STOPLIGHT_WHATSAPP_NUMBER = "whatsapp:+5521933007184"
 TEST_NUMBERS = ["+16265887741", "+41786914152"]
